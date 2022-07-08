ddb6e5b9d95ab1d050a3c5372de11159af66fea6
Leon <leongrdic@gmail.com>
2022-07-08 15:42:37 +0200

    support for `where x in ()` queries

--- src/Query/Filter/WhereFilterTrait.php
+++ src/Query/Filter/WhereFilterTrait.php
@@ -11,6 +11,14 @@ trait WhereFilterTrait
 
     use WhereTrait;
 
+    protected static function prepareValue(string|int|float|bool|null $value): string
+    {
+        if (is_string($value)) $value = json_encode($value); // quote and escape string
+        else if (is_bool($value)) $value = $value ? 'true' : 'false'; // convert boolean to string
+        else if (is_null($value)) $value = 'null'; // convert null to string
+        return (string) $value;
+    }
+
     protected static function genWhereExpression(array $conditions): string
     {
         $exp = '';
@@ -23,15 +31,16 @@ trait WhereFilterTrait
                 $exp .= static::genWhereExpression($condition->getConditions());
             } else
             if($condition instanceof WhereCondition) {
-                $value = $condition->value;
-                if(is_string($value)) $value = json_encode($value); // quote and escape string
-                else if(is_bool($value)) $value = $value ? 'true' : 'false'; // convert boolean to string
-                else if(is_null($value)) $value = 'null'; // convert null to string
+                if(in_array($condition->operand, ['IN', 'NOT IN'])) {
+                    $values = implode(',', array_map(static::prepareValue(...), $condition->value));
+                    if($condition->operand === 'NOT IN') $exp .= '!';
+                    $exp .= "dba_in_array($condition->column, [$values])";
+                    continue;
+                }
 
                 $operand = $condition->operand;
-                if($operand === '<=>' || $operand === '=') $operand = '=='; // achieve mysql-like behavior
-
-                $exp .= "$condition->column $operand $value";
+                if(in_array($operand, ['<=>', '='])) $operand = '=='; // achieve mysql-like behavior
+                $exp .= "$condition->column $operand {static::prepareValue($condition->value)}";
             }
         }
 
@@ -43,7 +52,7 @@ trait WhereFilterTrait
         $exp = static::genWhereExpression($this->conditions);
         if($exp === '') return $table;
 
-        $el = new SMPLang;
+        $el = new SMPLang([ 'dba_in_array' => in_array(...) ]);
         foreach($table->getRows() as $index => $row){
             $result = $el->evaluate($exp, array_combine($table->getColumnNames(), $row));
             if($result === false) $table->deleteRow($index);
